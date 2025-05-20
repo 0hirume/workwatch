8bd355e09f2e20b778dc8837ed60140e5dc4dc56
ohirume <0uhirume@gmail.com>
2025-05-20 23:28:43 +0800

    feat: split human time to compact and verbose

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ impl WorkWatcherApp {
                     }
                 }
             } else {
-                self.time = self.time.saturating_add(6401);
+                self.time = self.time.saturating_add(1);
             }
         }
         Ok(())
@@ -39,25 +39,43 @@ impl WorkWatcherApp {
         frame.render_widget(self, frame.area());
     }
 
-    fn get_human_time(&self) -> String {
+    fn get_compact_time(&self) -> String {
         let total = self.time;
         let sec = total % 60;
         let min = (total / 60) % 60;
-        let hr = (total / 3600) % 24;
+        let hr = (total / 3_600) % 24;
+        let days = total / 86_400;
+
+        if days > 0 {
+            format!("{}:{:02}:{:02}:{:02}", days, hr, min, sec)
+        } else if hr > 0 {
+            format!("{:02}:{:02}:{:02}", hr, min, sec)
+        } else if min > 0 {
+            format!("{:02}:{:02}", min, sec)
+        } else {
+            format!("{:02}", sec)
+        }
+    }
+
+    fn get_verbose_time(&self) -> String {
+        let total = self.time;
+        let sec = total % 60;
+        let min = (total / 60) % 60;
+        let hr = (total / 3_600) % 24;
         let days = total / 86_400;
 
         match (days, hr, min) {
             (d, _, _) if d > 0 => {
-                format!("{} Days - {:02}:{:02}:{:02}", d, hr, min, sec)
+                format!("{} Days, {} Hours, {} Minutes, {} Seconds", d, hr, min, sec)
             }
             (_, h, _) if h > 0 => {
-                format!("{:02}:{:02}:{:02}", h, min, sec)
+                format!("{} Hours, {} Minutes, {} Seconds", h, min, sec)
             }
             (_, _, m) if m > 0 => {
-                format!("{:02}:{:02}", m, sec)
+                format!("{} Minutes, {} Seconds", m, sec)
             }
             _ => {
-                format!("{:02}", sec)
+                format!("{} Seconds", sec)
             }
         }
     }
@@ -71,7 +89,7 @@ impl Widget for &WorkWatcherApp {
     fn render(self, area: Rect, buf: &mut Buffer) {
         Paragraph::new(Line::from(format!(
             "Logged Time: {}",
-            self.get_human_time()
+            self.get_compact_time()
         )))
         .block(Block::bordered().title(Line::from(" Work Watcher ")))
         .render(area, buf);
@@ -83,5 +101,6 @@ fn main() -> io::Result<()> {
     let mut work_watcher_app = WorkWatcherApp::default();
     let result = work_watcher_app.run(&mut terminal);
     ratatui::restore();
+    println!("Total Logged Time: {}", work_watcher_app.get_verbose_time());
     result
 }
